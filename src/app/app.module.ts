d8f2ae54809bad8b96d379f11c8899c9876dd81c
Kaushik Chaubal <kaushikchaubal@gmail.com>
2017-06-26 19:25:54 +0100

    Adding routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { UserViewComponent } from './user-view/user-view.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'user', component: UserViewComponent},
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    HomePageComponent,
+    UserViewComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
